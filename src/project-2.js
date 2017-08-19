143600aa20c0a14c5ea0c5c3c9b8e580476eb879
phytertek <ryan.phytertek@gmail.com>
2017-08-19 18:33:12 -0400

    condensed functions to one line where possible, refactored isPrime to utilize Sieve of Eratosthenes

--- src/project-2.js
+++ src/project-2.js
@@ -1,23 +1,9 @@
+/* eslint-disable no-confusing-arrow */
 // Do not change any of the function names
 
-const getBiggest = (x, y) => {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
-  if (x === y) {
-    return x;
-  }
-  if (x > y) {
-    return x;
-  }
-  return y;
-};
+const getBiggest = (x, y) => x >= y ? x : y;
 
 const greeting = (language) => {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'Spanish' -> 'Hola!'
-  // language: 'Chinese' -> 'Ni Hao!'
-  // if language is undefined return 'Hello!'
   switch (language) {
     case 'German':
       return 'Guten Tag!';
@@ -30,32 +16,13 @@ const greeting = (language) => {
   }
 };
 
-const isTenOrFive = (num) => {
-  // return true if num is 10 or 5
-  // otherwise return false
-  return num === 10 || num === 5;
-};
+const isTenOrFive = num => num === 10 || num === 5;
 
-const isInRange = (num) => {
-  // return true if num is less than 50 and greater than 20
-  return num < 50 && num > 20;
-};
+const isInRange = num => num < 50 && num > 20;
 
-const isInteger = (num) => {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
-  return Math.abs(Math.floor(num)) > 0 || num === 0;
-};
+const isInteger = num => Math.abs(Math.floor(num)) > 0 || num === 0;
 
 const fizzBuzz = (num) => {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
   if (num % 3 === 0 && num % 5 === 0) {
     return 'fizzbuzz';
   } else if (num % 3 === 0) {
@@ -67,90 +34,47 @@ const fizzBuzz = (num) => {
 };
 
 const isPrime = (num) => {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
-  if (num === 0) {
-    return false;
+  let divisor = 3;
+  const limit = Math.sqrt(num);
+
+  if (num === 2 || num === 1) return false;
+  if (num === 3) return true;
+  if (num % 2 === 0) return false;
+
+  while (divisor <= limit) {
+    if (num % divisor === 0) return false;
+    divisor += 2;
   }
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return num !== 1;
+  return true;
 };
 
-const returnFirst = (arr) => {
-  // return the first item from the array
-  return arr[0];
-};
+const returnFirst = arr => arr[0];
 
-const returnLast = (arr) => {
-  // return the last item of the array
-  return arr[arr.length - 1];
-};
+const returnLast = arr => arr[arr.length - 1];
 
-const getArrayLength = (arr) => {
-  // return the length of the array
-  return arr.length;
-};
+const getArrayLength = arr => arr.length;
 
-const incrementByOne = (arr) => {
-  // arr is an array of integers
-  // increase each integer by one
-  // return the array
-  return arr.map(i => i + 1);
-};
+const incrementByOne = arr => arr.map(i => i + 1);
 
 const addItemToArray = (arr, item) => {
-  // add the item to the end of the array
-  // return the array
   arr.push(item);
   return arr;
 };
 
 const addItemToFront = (arr, item) => {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
   arr.unshift(item);
   return arr;
 };
 
-const wordsToSentence = (words) => {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
-  return words.join(' ');
-};
+const wordsToSentence = words => words.join(' ');
 
-const contains = (arr, item) => {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
-  return arr.indexOf(item) === 0;
-};
+const contains = (arr, item) => arr.indexOf(item) !== -1;
 
-const addNumbers = (numbers) => {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
-  return numbers.reduce((acc, cv) => acc + cv);
-};
+const addNumbers = numbers => numbers.reduce((acc, cv) => acc + cv);
 
-const averageTestScore = (testScores) => {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
-  return testScores.reduce((acc, cv) => acc + cv) / testScores.length;
-};
+const averageTestScore = testScores => testScores.reduce((acc, cv) => acc + cv) / testScores.length;
 
-const largestNumber = (numbers) => {
-  // numbers is an array of integers
-  // return the largest integer
-  return Math.max.apply(null, numbers);
-};
+const largestNumber = numbers => Math.max.apply(null, numbers);
 
 // Do not modify code below this line.
 // --------------------------------
